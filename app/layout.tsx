806dc7e0794d80bf1e87845f484e35ab44ba6fef
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2024-06-18 19:59:16 +0700

    Logic Modal in SignUp

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
-          <Modal title="Hello World" isOpen />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
